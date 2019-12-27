97bd0152c89822d6b6b832d3199f121510c907e0
Jeffrey Vaz <jeff.j.vaz@gmail.com>
2019-12-27 17:57:23 -0500

    add alternative menu bar (just testing things out visually. Only one menu bar should be on a real website)

--- project-manager/src/components/NavBar.vue
+++ project-manager/src/components/NavBar.vue
@@ -8,6 +8,25 @@
           <span>Mule</span>
         </v-toolbar-title>
         <v-spacer></v-spacer>
+
+      <!-- Alternative dropdown menu option -->
+        <div class="text-center">
+          <v-menu offset-y>
+            <template v-slot:activator="{ on }">
+              <v-btn text color="grey" v-on="on">
+                <v-icon left>expand_more</v-icon>
+                <span>Menu (alternative)</span>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+                <v-list-item-title>{{ link.text }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </div>
+
+
         <v-btn text color="grey">
           <span>Sign Out</span>
           <v-icon right>exit_to_app</v-icon>

e5fcab4f178227c7167d8e4a527844bb646f4028
Jeffrey Vaz <jeff.j.vaz@gmail.com>
2019-12-27 11:42:22 -0500

    add loop of list items in side navigation drawer

--- project-manager/src/components/NavBar.vue
+++ project-manager/src/components/NavBar.vue
@@ -14,7 +14,16 @@
         </v-btn>
       </v-toolbar>
       <v-navigation-drawer v-model="drawer" app temporary hide-overlay class="primary">
-        <p>test</p>
+        <v-list>
+          <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+            <v-list-item-icon>
+              <v-icon class="white--text">{{ link.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
 
       </v-navigation-drawer>
 
@@ -36,7 +45,12 @@ export default {
   // }),
   data () {
     return {
-      drawer: false
+      drawer: false,
+      links: [
+        { icon: 'dashboard', text: 'Dashboard', route: '/' },
+        { icon: 'folder', text: 'My Projects', route: '/projects' },
+        { icon: 'person', text: 'Team', route: '/team' },
+      ],
     } 
   }
 };

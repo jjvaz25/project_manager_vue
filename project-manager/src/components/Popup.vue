4bb71650285df178213cf2dcd83b306e671ed77b
Jeffrey Vaz <jeff.j.vaz@gmail.com>
2019-12-27 19:43:20 -0500

    add title, information, and date picker to 'add new project form'. Values are being logged to console successfully

--- project-manager/src/components/Popup.vue
+++ project-manager/src/components/Popup.vue
@@ -10,16 +10,41 @@
         <v-card-title>
           <h2>Add a new project</h2>
         </v-card-title>
+        <v-card-text>
+          <v-form px-3>
+            <v-text-field label="Title" v-model="title" prepend-icon="folder"></v-text-field>
+            <v-textarea label="Information" v-model="content" prepend-icon="edit"></v-textarea>
 
+            <v-menu v-model="menu2" :close-on-content-click="false" transition="scale-transition" offset-y>
+              <template v-slot:activator="{ on }">
+                <v-text-field readonly v-model="date" label="Due date" prepend-icon="event" v-on="on"></v-text-field>
+              </template>
+              <v-date-picker v-model="date" @input="menu2 = false"></v-date-picker>
+            </v-menu>
+
+            <v-btn @click="submit" text class="success mx-0 mt-3">Add project</v-btn>
+          </v-form>
+        </v-card-text>
       </v-card>
-
-
     </v-dialog>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      title: '',
+      content: '',
+      menu2: false,
+      date: new Date().toISOString().substr(0,10)
+    }
+  },
+  methods: {
+    submit() {
+      console.log(this.title, this.content, this.date)
+    }
+  }
   
 }
 </script>
